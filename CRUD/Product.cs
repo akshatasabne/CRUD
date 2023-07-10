6f813a43225072801bd7cc5cfc5d162ca4e9d81c
Akshata <Akshata@LAPTOP-4CVBJOT7>
2023-07-10 17:53:33 +0530

    Add project files.

---
+++ CRUD/Product.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD
+{
+    public class Product
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public double Price { set; get; }
+    }
+
+    public class ProductCRUD
+    {
+        private List<Product> products;
+
+        public ProductCRUD()
+        {
+            products = new List<Product>();
+        }
+
+        public void AddProduct(Product prod)
+        {
+            products.Add(prod);
+        }
+
+        public List<Product> GetProduct()
+        {
+            return products;
+        }
+
+        public void UpdateProduct(Product prod)
+        { 
+            foreach(Product product in products)
+            {
+                if(product.Id == prod.Id)
+                {
+                    product.Name= prod.Name;
+                    product.Price= prod.Price;
+                    break;
+                }
+            }
+        }
+
+        public void DeleteProduct(int id)
+        {
+            foreach(Product product in products)
+            {
+                if( product.Id == id)
+                {
+                    products.Remove(product);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ CRUD/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductCRUD crud = new ProductCRUD();
+            int option = 0;
+            do
+            {
+                Console.WriteLine("1.Add Product Details");
+                Console.WriteLine("2.Update Product Details");
+                Console.WriteLine("3.Delete Product Details");
+                Console.WriteLine("4.Display Product Datails");
+                int op = Convert.ToInt32(Console.ReadLine());
+
+                switch (op)
+                {
+                    case 1:
+                        Product p = new Product();
+                        Console.WriteLine("Enter Product Id");
+                        p.Id = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Product Name");
+                        p.Name = Console.ReadLine();
+                        Console.WriteLine("Enter Product Price");
+                        p.Price = Convert.ToDouble(Console.ReadLine());
+
+                        crud.AddProduct(p);
+                        break;
+
+                    case 2:
+                        Product p1 = new Product();
+                        Console.WriteLine("Enter Product Id");
+                        p1.Id = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Product Name");
+                        p1.Name = Console.ReadLine();
+                        Console.WriteLine("Enter Product Price");
+                        p1.Price = Convert.ToDouble(Console.ReadLine());
+
+                        crud.UpdateProduct(p1);
+                        break;
+
+                    case 3:
+
+                        Console.WriteLine("Enter Id to Delete");
+                        int id = Convert.ToInt32(Console.ReadLine());
+                        crud.DeleteProduct(id);
+                        break;
+
+                    case 4:
+                        List<Product> list = crud.GetProduct();
+                        foreach (Product P in list)
+                        {
+                            Console.WriteLine($"{P.Id}-{P.Name}-{P.Price}");
+                        }
+                        break;
+                
+                }
+                Console.WriteLine("Enter 1 to Continue");
+                option=Convert.ToInt32(Console.ReadLine());
+            } while (option == 1);
+        }
+    }
+}
